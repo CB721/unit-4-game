5c62e92459d80f3b8713edf9cdbd8e75c3489339
Clint Brodar <clintbrodar@D-10-18-239-171.dhcp4.washington.edu>
2019-04-16 19:46:23 -0700

    Reworked html and css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,35 +32,60 @@ $(document).ready(function () {
         }
     ];
 
-    var displayCharacters = function () {
+//     var displayCharacters = function () {
 
-        for (var i = 0; i < characters.length; i++) {
+//         for (var i = 0; i < characters.length; i++) {
 
-            var charName = $("<h5></h5>");
-            charName.append(characters[i].name);
-            $("#players-profile").append(charName);
+//             var charName = $("<h5></h5>");
+//             charName.append(characters[i].name);
+//             $("#players-profile").append(charName);
 
-            var charProfile = $("<img>");
-            charProfile.attr("src", characters[i].profile);
-            charProfile.append(characters[i].profile);
-            $("#players-profile").append(charProfile);
+//             var charProfile = $("<img>");
+//             charProfile.attr("src", characters[i].profile);
+//             charProfile.append(characters[i].profile);
+//             $("#players-profile").append(charProfile);
 
-            var charHealth = $("<h6></h6>");
-            charHealth.append(characters[i].health);
-            $("#players-profile").append(charHealth);
+//             var charHealth = $("<h6></h6>");
+//             charHealth.append(characters[i].health);
+//             $("#players-profile").append(charHealth);
 
-        };
+//         };
 
-    };
+//     };
 
-    displayCharacters();
+//     displayCharacters();
 
-    var selectedChar;
-    var selectedDefender;
-    var waitingDefenders;
+//     var selectedChar;
+//     var selectedDefender;
+//     var waitingDefenders;
 
 
+// });
+
+var displayCharacters = function () {
+
+        var createLuke = $("#luke");
+        createLuke.append(characters[0].name);
+        $("#luke").append(createLuke);
+        var lukePicture = $("<img>");
+        lukePicture.attr("src", characters[0].profile);
+        $("#luke").append(lukePicture);
+        var lukeHealth = $("luke-health");
+        lukeHealth.attr("src", characters[0].health);
+        $("#luke-health").append(lukeHealth);
+
+
+};
+
+displayCharacters();
+
+
+$("#luke").on("click", function () {
+    var userPick;
+    console.log(userPick);
+    $("#players-profile").appendTo("#players-selection");
 });
+
 //characters have 3 variables
 //Health Points
 //each character must be different
@@ -73,16 +98,16 @@ $(document).ready(function () {
 //never changes
 
 //each character needs to be its own variable
-$()
+
 
 //when that character is clicked, it is moved to player selection
 //other characters are moved to enemies
 //user clicks enemy to move to defender area
 
-$("#attack-button").on("click", function () {
+// $("#attack-button").on("click", function () {
     //attack defender
     //increase attack by base number (6, 12, 18 etc.)
-});
+// });
 
 //user must be able to win with each player
 
@@ -91,11 +116,11 @@ $("#attack-button").on("click", function () {
 
 //user selects character by selecting the image
 
-$("#players-profile").on("click", function () {
-    var userPick;
-    console.log(userPick);
-    $("#players-profile").appendTo("#players-selection");
-});
+// $("#players-profile").on("click", function () {
+//     var userPick;
+//     console.log(userPick);
+//     $("#players-profile").appendTo("#players-selection");
+// });
     //sound is played on selection
     //character goes to 'Your Character' section
     //other characters go to 'Enemies Available to Attack' section
@@ -119,3 +144,4 @@ $("#players-profile").on("click", function () {
 //once all enemies are defeated
     //win sound is triggered
     //you win alert
+});
